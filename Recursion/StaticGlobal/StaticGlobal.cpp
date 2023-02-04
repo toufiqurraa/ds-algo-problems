deaf94cce6ae8329055b7582f7fdf81eab368912
Toufiqur Rahman <toufiqurraa@gmail.com>
2023-02-04 19:28:07 +0600

    static value and recursion

---
+++ Recursion/StaticGlobal/StaticGlobal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int n) {
+    static int x = 0;
+
+    if (n > 0) {
+        x++;
+
+        return func(n - 1) + x;
+    }
+
+    return 5;
+}
+
+int main()
+{
+    int r;
+
+    r = func(5);
+
+    cout << r;
+
+    return 0;
+}
+
