01d62881607f56396f8ab8a21c8b9c0073eec3c4
Toufiqur Rahman <toufiqurraa@gmail.com>
2023-02-04 20:56:38 +0600

    tree recursion

---
+++ Recursion/TreeRecursion/TreeRecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void treeRecursion(int n) {
+    if (n > 0) {
+        cout << n << " ";
+
+        treeRecursion(n - 1);
+        treeRecursion(n - 1);
+    }
+}
+
+int main()
+{
+    treeRecursion(3);
+
+    return 0;
+}
+
